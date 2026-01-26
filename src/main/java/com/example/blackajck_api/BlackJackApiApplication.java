8539d723df8a0d3cfeb499c3d973c0f253880560
YoussefEG09 <youssef.eg09@gmail.com>
2026-01-26 11:09:27 +0100

    first commit

---
+++ src/main/java/com/example/blackajck_api/BlackJackApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackajck_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackJackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackJackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blackajck_api/BlackJackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackajck_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackJackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
