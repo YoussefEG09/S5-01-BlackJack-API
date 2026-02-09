c8b70036319a84c6d9a617e5cf498d6ff456056f
YoussefEG09 <youssef.eg09@gmail.com>
2026-02-09 10:41:19 +0100

    feat: Card modified to make relation with GameMapper

--- src/main/java/com/example/blackajck_api/domain/model/Card.java
+++ src/main/java/com/example/blackajck_api/domain/model/Card.java
@@ -3,6 +3,8 @@ package com.example.blackajck_api.domain.model;
 import com.example.blackajck_api.domain.model.enums.Rank;
 import com.example.blackajck_api.domain.model.enums.Suit;
 
+import java.util.Arrays;
+
 public class Card {
 
     private final Suit suit;
@@ -15,6 +17,21 @@ public class Card {
 
     public int getValue() {
         return rank.getValue();
+    }
+
+    public static Card fromString(String value) {
+        // Ej: "ACE♠"
+        String rankPart = value.substring(0, value.length() - 1);
+        String suitSymbol = value.substring(value.length() - 1);
+
+        Rank rank = Rank.valueOf(rankPart);
+
+        Suit suit = Arrays.stream(Suit.values())
+                .filter(s -> s.getSymbol().equals(suitSymbol))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid suit"));
+
+        return new Card(suit, rank);
     }
 
     @Override
